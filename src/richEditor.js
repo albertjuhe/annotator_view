8cd4dcd0c7c3851f96cbf7a868fc8801e3df4a93
Albert Juhe Brugué <albertjuhe@gmail.com>
2016-09-20 08:52:12 +0200

    Update richEditor.js

--- src/richEditor.js
+++ src/richEditor.js
@@ -1,3 +1,19 @@
+/*
+Annotator view panel Plugin v1.0 (https://https://github.com/albertjuhe/annotator_view/)
+Copyright (C) 2014 Albert Juhé Brugué
+License: https://github.com/albertjuhe/annotator_view/License.rst
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
 var _ref,
   __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
   __hasProp = {}.hasOwnProperty,
